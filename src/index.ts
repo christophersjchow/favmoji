5f98a9413ef6914c0088d836a56b3437cef488e7
Christopher Chow <chris@chowie.net>
2023-01-20 12:20:45 +1100

    ref: clean up html reponse using basic TailwindCSS

--- src/index.ts
+++ src/index.ts
@@ -16,22 +16,32 @@ export default {
     if (path.length === 0) {
       return new Response(
         `
-        <head>
-          <title>favmoji</title>
-          <meta charset="utf-8">
-          <link rel="icon" href="${domain}/🌸" />
-        </head>
-        <body>
-          <pre>
-            <p>example request: <a href="${domain}🌸">${domain}/🌸</a></p>
-            <p>example usage as favicon: &lt;link rel="icon" href="${domain}/🌸" /&gt;</p>
-          </pre>
-        </body>
-        <style>
-          a {
-            text-decoration: none;
-          }
-        </style>
+        <!doctype html>
+        <html>
+          <head>
+            <title>Favmoji</title>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script src="https://cdn.tailwindcss.com"></script>
+            <link rel="icon" href="/💻️" />
+          </head>
+          <body>
+            <section class="bg-white dark:bg-gray-900">
+              <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div class="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                  <h2 class="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+                    Favmoji
+                  </h2>
+                  <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+                    Use emojis (Twemoji) as a favicon in your project.
+                  </p>
+
+                  <p class="font-mono whitespace-pre pt-20">&lt;link rel=&quot;icon&quot; href=&quot;https://yourdomain.workers.dev/&lt;emoji&gt;&quot; /&gt;</p>
+                </div>
+              </div>
+            </section>
+          </body>
+        </html>
         `,
         {
           headers: {
